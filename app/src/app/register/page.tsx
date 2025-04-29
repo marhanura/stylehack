bc086fd261492c37cca5f7103bd467bbe33368da
Marha Nur Amalina <marhanura@gmail.com>
2025-04-29 18:31:20 +0700

    Feat: Register/Login Page

---
+++ app/src/app/register/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Logo from "../../../public/logotext.png";
+import RegisterImage from "../../../public/register.webp";
+export default function RegisterPage() {
+  return (
+    <div className="flex flex-row h-screen bg-[#E7DFD1]">
+      <form className="flex flex-col flex-1 p-15 items-center justify-center">
+        <Image src={Logo} height={30} alt="Logo" className="mb-5" />
+        <h1 className="font-(family-name:--font-bodoni-moda) text-[28px]">
+          Register
+        </h1>
+        <input
+          className="input my-3 rounded-sm border-0"
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+        <input
+          className="input my-3 rounded-sm border-0"
+          type="email"
+          name="username"
+          placeholder="Username"
+        />
+        <input
+          className="input my-3 rounded-sm border-0"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <select className="select my-3 rounded-sm border-0 w-full max-w-xs">
+          <option disabled selected>
+            Select gender
+          </option>
+          <option>Female</option>
+          <option>Male</option>
+          <option>Others</option>
+        </select>
+        <button className="btn btn-secondary my-3 rounded-sm border-0 w-full max-w-xs">
+          Register
+        </button>
+      </form>
+      <div className="flex flex-1 justify-center items-center">
+        <Image
+          src={RegisterImage}
+          height={500}
+          alt="Register Image"
+          className="clip-image"
+        />
+      </div>
+    </div>
+  );
+}
