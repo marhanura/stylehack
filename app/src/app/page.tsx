2bd25ff2be032a15aa6f242a2b01f34d9b310906
Marha Nur Amalina <marhanura@gmail.com>
2025-04-29 20:38:47 +0700

    Feat: Profile page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,6 +3,9 @@ import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import MainImage from "../../public/main.png";
 import Image1 from "../../public/image1.webp";
+import Image2 from "../../public/image2.webp";
+import Image3 from "../../public/image3.webp";
+import Image4 from "../../public/image4.webp";
 
 export default function Home() {
   return (
@@ -17,14 +20,37 @@ export default function Home() {
           </div>
           <Image
             src={MainImage}
-            width={400}
+            width={450}
             alt="Main Image"
             className="z-10"
           />
         </div>
       </div>
-      <div className="h-screen bg-[#e7dfd1]">
-        <Image src={Image1} width={250} alt="Example" />
+      <div className="h-screen bg-[#e7dfd1] flex flex-row mx-10 items-center justify-center">
+        <Image
+          src={Image1}
+          width={250}
+          alt="Example"
+          className="clip-image mx-5"
+        />
+        <Image
+          src={Image2}
+          width={250}
+          alt="Example"
+          className="clip-image mx-5"
+        />
+        <Image
+          src={Image3}
+          width={250}
+          alt="Example"
+          className="clip-image mx-5"
+        />
+        <Image
+          src={Image4}
+          width={250}
+          alt="Example"
+          className="clip-image mx-5"
+        />
       </div>
     </div>
   );
