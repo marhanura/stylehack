972d0ee240cf9b42f025276a9d78410d10623dda
drewmps <andrewmarco97@gmail.com>
2025-04-29 21:32:20 +0700

    refactor: register - jadi memakai mongodb

--- app/src/db/models/UserModel.ts
+++ app/src/db/models/UserModel.ts
@@ -1,4 +1,6 @@
 import { getDB } from "../config/mongodb";
+import { hashPassword } from "../helpers/bcrypt";
+import CustomError from "../helpers/CustomError";
 
 export interface IUser {
   name: string;
@@ -19,5 +21,21 @@ export default class UserModel {
     const collection = this.getCollection();
     const users = await collection.findOne(payload);
     return users;
+  }
+
+  static async register(payload: IUser): Promise<string> {
+    const collection = this.getCollection();
+
+    let user = await collection.findOne({ email: payload.email });
+    if (user) {
+      throw new CustomError("Email must be unique", 400);
+    }
+
+    await collection.insertOne({
+      ...payload,
+      password: hashPassword(payload.password),
+    });
+
+    return "Successfully registered";
   }
 }
