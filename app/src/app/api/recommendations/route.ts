8fa43f37a8706e0d9f773c1115f11aba85cb0d50
drewmps <andrewmarco97@gmail.com>
2025-05-01 22:52:19 +0700

    feat: recommendation - get current user's recommendation

---
+++ app/src/app/api/recommendations/route.ts
@@ -0,0 +1,33 @@
+import CustomError from "@/db/helpers/CustomError";
+import RecomendationModel from "@/db/models/RecomendationModel";
+import { NextRequest, NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+export async function GET(req: NextRequest) {
+  try {
+    const userId = req.headers.get("x-user-id");
+    if (!userId) {
+      throw new CustomError("Unauthorized", 401);
+    }
+
+    const recommendations =
+      await RecomendationModel.getLoginUserRecommendations(userId);
+
+    return NextResponse.json({ recommendations });
+  } catch (err: unknown) {
+    if (err instanceof ZodError) {
+      const error = err.errors[0];
+      return NextResponse.json(
+        { message: `${error.path} - ${error.message}` },
+        { status: 400 }
+      );
+    } else if (err instanceof CustomError) {
+      return NextResponse.json(
+        { message: `${err.message}` },
+        { status: err.status }
+      );
+    }
+
+    return Response.json({ message: "ISE" }, { status: 500 });
+  }
+}
--- app/src/db/models/RecomendationModel.ts
+++ app/src/db/models/RecomendationModel.ts
@@ -1,22 +1,67 @@
 import { ObjectId } from "mongodb";
+import { getDB } from "../config/mongodb";
 
 export interface IRecomendation {
-  userId: ObjectId
-  products: IProduct[]
-  promptBy: IPrompt
+  userId: ObjectId;
+  products: IProduct[];
+  promptBy: IPrompt;
+  recommendationId: ObjectId;
 }
 
-interface IProduct{
-    category: string
-    name: string
-    links: string[]
+interface IProduct {
+  category: string;
+  name: string;
+  links: string[];
 }
 
-interface IPrompt{
-    type: string
-    input: string
+interface IPrompt {
+  type: string;
+  input: string;
 }
 
-export default class RecomendationModel{
-    
+export default class RecomendationModel {
+  static getCollection() {
+    const db = getDB();
+    return db.collection<IRecomendation>("Recommendations");
+  }
+
+  static async getLoginUserRecommendations(userId: string) {
+    const collection = this.getCollection();
+    const recommendations = await collection
+      .aggregate([
+        {
+          $match: {
+            userId: new ObjectId("6810dbfd2e7d4851742daf01"),
+          },
+        },
+        {
+          $lookup: {
+            from: "Recommendations",
+            localField: "_id",
+            foreignField: "recommendationId",
+            as: "extraRecommendation",
+          },
+        },
+        {
+          $unwind: {
+            path: "$extraRecommendation",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $match: {
+            recommendationId: {
+              $exists: false,
+            },
+          },
+        },
+        {
+          $project: {
+            "extraRecommendation.recommendationId": 0,
+          },
+        },
+      ])
+      .toArray();
+    return recommendations;
+  }
 }
