b34adc1c241ee6cd0a968e3d772d943b703a9e26
drewmps <andrewmarco97@gmail.com>
2025-05-01 14:29:47 +0700

    feat: wishlist - create wishlist

--- app/src/db/models/WishlistModel.ts
+++ app/src/db/models/WishlistModel.ts
@@ -1,6 +1,28 @@
 import { ObjectId } from "mongodb";
+import { getDB } from "../config/mongodb";
 
-export interface IWishlist{
-    userId: ObjectId
-    recomendationId: ObjectId
+export interface IWishlist {
+  userId: ObjectId;
+  recomendationId: ObjectId;
+}
+
+export default class WishlistModel {
+  static getCollection() {
+    const db = getDB();
+    return db.collection<IWishlist>("Wishlists");
+  }
+
+  static async findOne(recommendationId: ObjectId) {
+    const collection = this.getCollection();
+    const wishlist = await collection.findOne(recommendationId);
+    return wishlist;
+  }
+
+  static async createWishlist(payload: IWishlist): Promise<string> {
+    const collection = this.getCollection();
+
+    await collection.insertOne(payload);
+
+    return "Successfully added wishlist";
+  }
 }
