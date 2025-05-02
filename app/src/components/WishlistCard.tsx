dd1201650ca7d95c1c3f62a73b53da5f7cdce514
Marha Nur Amalina <marhanura@gmail.com>
2025-05-02 23:04:13 +0700

    Feat: Post and delete wishlist

--- app/src/components/WishlistCard.tsx
+++ app/src/components/WishlistCard.tsx
@@ -1,13 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import { IDetail } from "@/app/(auth)/lookbook/page";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 export interface IStyleCard {
   data: IDetail;
+  onDelete?: () => void;
 }
 
-export default function WishlistCard(data: IStyleCard) {
-  const wishlist: IDetail = data.data;
+export default function WishlistCard(props: IStyleCard) {
+  const router = useRouter();
+  const wishlist: IDetail = props.data;
+  const { onDelete } = props;
+
+  const removeWishlist = async (id: string) => {
+    Swal.fire({
+      text: "Remove from wishlist?",
+      icon: "warning",
+      showCancelButton: true,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await fetch(`/api/wishlists/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (!res.ok) {
+          const data = await res.json();
+          Swal.fire({
+            text: data.message,
+            icon: "error",
+          });
+          return;
+        }
+        Swal.fire({
+          text: "Removed from wishlist",
+          icon: "success",
+        });
+        if (onDelete) onDelete();
+        router.refresh();
+      }
+    });
+  };
 
   return (
     <div className="card bg-base-100 h-100 w-full rounded-none">
@@ -39,9 +75,30 @@ export default function WishlistCard(data: IStyleCard) {
           <Image src={Thumbnail} width={50} height={50} alt="Anak anak" />
           <Image src={Thumbnail} width={50} height={50} alt="Anak anak" />
         </div> */}
-        <Link href={`/wishlist/${wishlist._id}`} className="btn">
-          See Detail
-        </Link>
+        <div className="card-actions justify-end">
+          <Link href={`/wishlist/${wishlist._id}`} className="btn">
+            See Detail
+          </Link>
+          <button
+            className="btn btn-accent"
+            onClick={() => removeWishlist(wishlist._id.toString())}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
