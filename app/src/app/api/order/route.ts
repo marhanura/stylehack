2aa79fcb98c7f0b7049605c1a3b210dfba50ef25
Hilmi Ahmad <hilmiadd@gmail.com>
2025-05-03 12:26:40 +0700

    feat: add get logged in user order endpoint

--- app/src/app/api/order/route.ts
+++ app/src/app/api/order/route.ts
@@ -4,6 +4,26 @@ import UserModel from "@/db/models/UserModel";
 import { ObjectId } from "mongodb";
 import { NextRequest } from "next/server";
 
+export async function GET(request: NextRequest) {
+  try {
+    const _id = request.headers.get("x-user-id");
+    if (!_id) {
+      throw new CustomError("Unauthorized", 401);
+    }
+
+    const order = await OrderModel.find(new ObjectId(_id));
+    return Response.json(order);
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return Response.json(
+        { message: error.message },
+        { status: error.status }
+      );
+    }
+    return Response.json({ message: "ISE" }, { status: 500 });
+  }
+}
+
 export async function POST(request: NextRequest) {
   try {
     const _id = request.headers.get("x-user-id");
@@ -12,9 +32,9 @@ export async function POST(request: NextRequest) {
     }
     const userId = new ObjectId(_id);
     const { insertedId } = await OrderModel.createOrder(userId);
-    const user = await UserModel.findOne({_id: userId})
-    if(!user || !user._id || !user.email){
-      throw new CustomError("User not found", 404)
+    const user = await UserModel.findOne({ _id: userId });
+    if (!user || !user._id || !user.email) {
+      throw new CustomError("User not found", 404);
     }
 
     // let snap = new midtransClient.Snap({
@@ -35,14 +55,18 @@ export async function POST(request: NextRequest) {
     // };
 
     // const midtransToken = await snap.createTransaction(parameter);
-    const data = await OrderModel.createMidtransTransaction(insertedId.toString(), user.name, user.email);
+    const data = await OrderModel.createMidtransTransaction(
+      insertedId.toString(),
+      user.name,
+      user.email
+    );
 
-    if(!data.redirect_url){
-      throw new CustomError("Midtrans error", 500)
+    if (!data.redirect_url) {
+      throw new CustomError("Midtrans error", 500);
     }
-    await OrderModel.setLink(data.redirect_url, insertedId)
+    await OrderModel.setLink(data.redirect_url, insertedId);
 
-    return Response.json({orderId: insertedId});
+    return Response.json({ orderId: insertedId });
   } catch (error: unknown) {
     console.log("🐄 - POST - error:", error);
     if (error instanceof CustomError) {
