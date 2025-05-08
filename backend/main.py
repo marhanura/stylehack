ea49d885d3813c6e8605d109436bb54f9619f328
Wanda Budiono <wandaabudiono@gmail.com>
2025-05-08 12:58:58 +0700

    Chore : Fixing ai

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from api import recommend
+import uvicorn
+import os
 
 app = FastAPI()
 app.include_router(recommend.router)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
